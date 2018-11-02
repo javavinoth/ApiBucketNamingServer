2f33e1ee6254081f92284f46774c95300e3b1d6d
javavinoth <vinoth.sakthivel95@gmail.com>
2018-11-02 11:31:08 +0530

    First Commit

---
+++ apibucket-naming-server/src/test/java/com/apibucket/naming/server/ApibucketNamingServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.apibucket.naming.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApibucketNamingServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
